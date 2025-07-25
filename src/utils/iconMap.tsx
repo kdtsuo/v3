b82b733897a18dce04c71e82f8104649ab1e0e1a
rin-windows <phurinmeng@gmail.com>
2025-07-24 22:22:30 -0700

    feat: add YouTube logo to icon map

--- src/utils/iconMap.tsx
+++ src/utils/iconMap.tsx
@@ -2,6 +2,7 @@ import { Link, Music, TicketMinus, LucideIcon } from "lucide-react";
 import rubricLogo from "@/assets/img/icons/rubriclogo.png";
 import discordColorLogo from "@/assets/img/icons/discordcolorlogo.png";
 import googleFormsLogo from "@/assets/img/icons/googleformslogo.png";
+import youtubelogo from "@/assets/img/icons/youtubelogo.png";
 
 // Define a type for our icon map
 interface IconMapItem {
@@ -12,6 +13,7 @@ interface IconMapItem {
 // Map string identifiers to their respective icon components or image paths
 const iconMap: Record<string, IconMapItem> = {
   link: { iconComponent: Link },
+  youtube: { imagePath: youtubelogo },
   rubric: { imagePath: rubricLogo },
   discord: { imagePath: discordColorLogo },
   googleForms: { imagePath: googleFormsLogo },
