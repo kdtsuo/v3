09e78cbdb5144de2e9436b3daec5d58d78a1e50c
rin-windows <phurinmeng@gmail.com>
2025-11-02 23:29:54 -0800

    fix: add TypeScript error suppression for image imports in Discover component

--- src/components/Discover.tsx
+++ src/components/Discover.tsx
@@ -1,7 +1,11 @@
 import { DiscoverCard } from "@/components/subcomponents";
+// @ts-expect-error this is needed to ignore the error for the image imports
 import teamphoto from "@/assets/img/stock/teamphoto.jpeg?w=800&format=webp&quality=80";
+// @ts-expect-error this is needed to ignore the error for the image imports
 import joinourteam from "@/assets/img/stock/joinourteam.jpeg?w=800&format=webp&quality=80";
+// @ts-expect-error this is needed to ignore the error for the image imports
 import showcase from "@/assets/img/stock/showcase.jpeg?w=800&format=webp&quality=80";
+// @ts-expect-error this is needed to ignore the error for the image imports
 import events from "@/assets/img/stock/events.jpeg?w=800&format=webp&quality=80";
 import { BetweenHorizonalStart, Contact, HandCoins, Info } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui";
